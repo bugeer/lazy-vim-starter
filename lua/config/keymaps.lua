2527d16857c935fa0a0c5ef0e392767579707e1f
Calcite <calcite@CalciteKe.local>
2024-08-15 11:04:18 +0800

    add tab prev/next on mac

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,5 +6,13 @@ vim.keymap.del("n", "H")
 vim.keymap.del("n", "L")
 vim.keymap.del("n", "<A-j>")
 vim.keymap.del("n", "<A-k>")
-vim.keymap.set("n", "<A-h>", "<cmd>bprevious<cr>")
-vim.keymap.set("n", "<A-l>", "<cmd>bnext<cr>")
+
+local uname = vim .loop.os_uname();
+
+if uname.sysname == "Darwin" then
+    vim.keymap.set("n", "<M-h>", "<cmd>bprevious<cr>")
+    vim.keymap.set("n", "<M-l>", "<cmd>bnext<cr>")
+else
+    vim.keymap.set("n", "<A-h>", "<cmd>bprevious<cr>")
+    vim.keymap.set("n", "<A-l>", "<cmd>bnext<cr>")
+end
