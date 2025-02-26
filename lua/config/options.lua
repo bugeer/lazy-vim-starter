bdf1b967d1125519fca28dc47df94d93bfb09260
zhangjing529 <zhangjing529@ke.com>
2025-02-26 10:56:35 +0800

    remove fileencodings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,7 +14,7 @@ vim.opt.shiftwidth = 4
 vim.opt.foldmethod = "indent"
 vim.opt.foldlevel  = 99
 
-vim.opt.fileencodings = { "utf=8", "chinese", "latin-1", "gbk", "gb18030", "gk2312", "cp936" }
+-- vim.opt.fileencodings = { "utf=8", "chinese", "latin-1", "gbk", "gb18030", "gk2312", "cp936" }
 
 vim.opt.listchars = {
   trail    = "┌",
