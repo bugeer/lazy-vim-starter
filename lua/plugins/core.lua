ac68b17dbac699196ce760f1920b60685ff9a353
zhangjing529 <zhangjing529@ke.com>
2026-09-04 17:47:49 +0800

    update v0.12.*

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,8 +1,18 @@
 return {
   {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    main = "catppuccin",
+    opts = {
+      flavour = "mocha",
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
-    }
-  }
+      colorscheme = "catppuccin-mocha",
+    },
+  },
 }
---
+++ lua/plugins/snacks-image.lua
@@ -0,0 +1,11 @@
+-- ~/.config/nvim/lua/plugins/snacks-image.lua
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      image = {
+        enabled = true,
+      },
+    },
+  },
+}
